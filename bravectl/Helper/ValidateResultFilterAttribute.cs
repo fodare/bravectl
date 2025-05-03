ecaff7953ffdfc6ecfd1278f629efe51fe626e33
fodare <folorunsodamilare007@hotmail.com>
2025-05-03 22:47:44 +0200

    Initial query parameters custom validation attributes.

---
+++ bravectl/Helper/ValidateResultFilterAttribute.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Bravectl.Helper
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class ValidateResultFilterAttribute : ValidationAttribute
+    {
+        private static readonly string[] knownResultFilter = ["discussions", "faq", "infobox", "news", "query", "summarizer", "videos", "web", "locations"];
+        public override bool IsValid(object? value)
+        {
+            if (value is string && !string.IsNullOrWhiteSpace((string?)value))
+            {
+                if (knownResultFilter.Contains(((string?)value)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
