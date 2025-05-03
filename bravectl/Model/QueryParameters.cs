a1d98e6a71155a0c0fe04fc972af5600ce22444f
fodare <folorunsodamilare007@hotmail.com>
2025-05-03 22:48:26 +0200

    Add default values and custom validation attributes.

--- bravectl/Model/QueryParameters.cs
+++ bravectl/Model/QueryParameters.cs
@@ -1,17 +1,28 @@
 using System.ComponentModel.DataAnnotations;
+using Bravectl.Helper;
 namespace BraveCtl.Model
 {
     public class QueryParameters
     {
         [StringLength(400, MinimumLength = 1, ErrorMessage = "Qurey string can not be longer than {1}.")]
         public string? Q { get; set; }
+
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Country code can not be lower / longer than {1} charcters.")]
         public string? Country { get; set; }
+
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Search language can not be lower /  longer the {1} charcters.")]
         public string? Search_language { get; set; }
+
+        [ValidCulture(ErrorMessage = "Provided culture / interface code is not valid. Example format de-DE.")]
         public string? UI_Language { get; set; }
         public int Count { get; set; } = 20;
+
+        [ValidateSafeSearch(ErrorMessage = "Provided safe search input is not valid. Possible values are off, moderate, strict.")]
         public string? SafeSearch { get; set; }
         public string? Freshness { get; set; }
         public bool Spellcheck { get; set; } = true;
+
+        [ValidateResultFilter(ErrorMessage = "Provided filter input is not valid. Possible values are discussions, faq, infobox, news, query, summarizer, videos, web, locations.")]
         public string? ResultFilter { get; set; }
         public bool Summary { get; set; } = true;
     }
