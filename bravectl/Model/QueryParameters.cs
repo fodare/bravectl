f876d12f6f7f9a6be358aafe6c5dcb78139fcfb8
fodare <folorunsodamilare007@hotmail.com>
2025-05-04 00:07:36 +0200

    Add default parameters.

--- bravectl/Model/QueryParameters.cs
+++ bravectl/Model/QueryParameters.cs
@@ -1,29 +1,28 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using Bravectl.Helper;
 namespace BraveCtl.Model
 {
     public class QueryParameters
     {
-        [StringLength(400, MinimumLength = 1, ErrorMessage = "Qurey string can not be longer than {1}.")]
+        [StringLength(400, MinimumLength = 2, ErrorMessage = "Qurey string can not be longer than {1} / lower than {2} characters.")]
         public string? Q { get; set; }
 
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "Country code can not be lower / longer than {1} charcters.")]
-        public string? Country { get; set; }
+        public string? Country { get; set; } = "US";
 
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "Search language can not be lower /  longer the {1} charcters.")]
-        public string? Search_language { get; set; }
+        public string? Search_language { get; set; } = "en";
 
-        [ValidCulture(ErrorMessage = "Provided culture / interface code is not valid. Example format de-DE.")]
-        public string? UI_Language { get; set; }
+        [ValidCulture(ErrorMessage = "Provided culture / interface code is not valid. Example expected format de-DE.")]
+        public string? UI_Language { get; set; } = "en-US";
         public int Count { get; set; } = 20;
 
         [ValidateSafeSearch(ErrorMessage = "Provided safe search input is not valid. Possible values are off, moderate, strict.")]
-        public string? SafeSearch { get; set; }
+        public string? SafeSearch { get; set; } = "off";
         public string? Freshness { get; set; }
         public bool Spellcheck { get; set; } = true;
 
         [ValidateResultFilter(ErrorMessage = "Provided filter input is not valid. Possible values are discussions, faq, infobox, news, query, summarizer, videos, web, locations.")]
-        public string? ResultFilter { get; set; }
+        public string? ResultFilter { get; set; } = "web";
         public bool Summary { get; set; } = true;
     }
 }
