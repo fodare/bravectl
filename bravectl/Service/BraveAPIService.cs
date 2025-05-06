302a4f823337919c089c9a872287e03da5e27f4f
fodare <folorunsodamilare007@hotmail.com>
2025-05-06 08:02:09 +0200

    Rename method name.

--- bravectl/Service/BraveAPIService.cs
+++ bravectl/Service/BraveAPIService.cs
@@ -14,7 +14,7 @@ namespace Bravectl.Service
             _braveAPIKey = Environment.GetEnvironmentVariable("braveAPIKey");
         }
 
-        public async Task<BraveResponse?> GetRequest(QueryParameters queryParameters)
+        public async Task<BraveResponse?> Search(QueryParameters queryParameters)
         {
             var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.search.brave.com/res/v1/web/search?q={queryParameters.Q}&country={queryParameters.Country}&search_lang={queryParameters.Search_language}&ui_lang={queryParameters.UI_Language}&safesearch={queryParameters.SafeSearch}&result_filter={queryParameters.ResultFilter}&count={queryParameters.Count}");
             request.Headers.Add("X-Subscription-Token", $"{_braveAPIKey}");
