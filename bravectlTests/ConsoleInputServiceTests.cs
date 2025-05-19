b57600a61d504588f2f854d419fba0dd178630d5
fodare <folorunsodamilare007@hotmail.com>
2025-05-19 13:26:04 +0200

    Add tests for submenus and adapt tests for Ansi-console.

--- bravectlTests/ConsoleInputServiceTests.cs
+++ bravectlTests/ConsoleInputServiceTests.cs
@@ -1,40 +1,73 @@
-﻿using Bravectl.Service;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Bravectl.Service;
 using BraveCtl.Model;
 using Moq;
+using Spectre.Console;
+using System.IO;
+using Spectre.Console.Rendering;
+using System.Text.RegularExpressions;
 
 namespace bravectlTests
 {
+    [Collection("Non-Parallel Collection")]
     public class ConsoleInputServiceTests
     {
-        private Mock<IBraveAPIService> _braveAPIService;
-        private ConsoleInputService _cut;
-        public ConsoleInputServiceTests()
+
+        [Fact]
+        public async Task Run_ShallPrint_HelpMessage_WhenCommandArgument_IsEmpty()
         {
-            _braveAPIService = new Mock<IBraveAPIService>();
-            _cut = new([], _braveAPIService.Object);
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            Mock<IBraveAPIService> _braveAPiService = new();
+            ConsoleInputService _cut = new([], _braveAPiService.Object, console);
+            string sampleConsoleMessage = "A lightweight C# command-line tool";
+
+            await _cut.Run();
+
+            var result = output.ToString().Trim();
+            string errorDescription = $"Expected '{sampleConsoleMessage}' is present in '{result}.'";
+            Assert.True(result.IndexOf(sampleConsoleMessage, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
         }
 
         [Theory]
         [InlineData("--filter", "-h")]
-        public async Task RunShallPrintHelpMessage_UserPassesIn_HelpFlag(params string[] cmdArguments)
+        public async Task Run_ShallPrintHelpMessage_WhenUserPassesIn_HelpFlag(params string[] cmdArguments)
         {
-            _cut = new ConsoleInputService(cmdArguments, _braveAPIService.Object);
-            StringWriter stringWriter = new();
-            Console.SetOut(stringWriter);
-            string sampleHelpMessageContent = "Print help information";
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            Mock<IBraveAPIService> _braveAPiService = new();
+            ConsoleInputService _cut = new(cmdArguments, _braveAPiService.Object, console);
+            string sampleConsoleMessage = "A lightweight C# command-line tool";
 
             await _cut.Run();
 
-            var output = stringWriter.ToString();
-            string errorDescription = $"Expect '{sampleHelpMessageContent}' is present in\n\n '{output.ToString()}'";
-            Assert.True(output.IndexOf(sampleHelpMessageContent, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
+            var result = output.ToString().Trim();
+            string errorDescription = $"Expect '{sampleConsoleMessage}' is present in\n\n '{output.ToString()}'";
+            Assert.True(result.IndexOf(sampleConsoleMessage, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
         }
 
         [Theory]
         [InlineData("--filter", "web", "-q", "What")]
+        [InlineData("--filter", "videos", "-q", "What")]
         public async Task ParseCommandLineArguments_ShallReturn_DictionaryOfAll_CmdArguments(params string[] cmdArguments)
         {
-            _cut = new ConsoleInputService(cmdArguments, _braveAPIService.Object);
+            Mock<IBraveAPIService> _braveAPIService = new Mock<IBraveAPIService>();
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
             int expectedParsedArgumentDictionaryCount = cmdArguments.Length / 2;
 
             Dictionary<string, string> parsedArguments = await _cut.ParseCommandLineArguments();
@@ -46,10 +79,17 @@ namespace bravectlTests
         [InlineData("--filter", "web", "-q", "What")]
         public async Task ConstructQueryParameters_ReturnsVlaid_QueryParametersObject(params string[] cmdArguments)
         {
-            _cut = new ConsoleInputService(cmdArguments, _braveAPIService.Object);
+            Mock<IBraveAPIService> _braveAPIService = new Mock<IBraveAPIService>();
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
 
             Dictionary<string, string> parsedArguments = await _cut.ParseCommandLineArguments();
-            QueryParameters queryParameters = await ConsoleInputService.ConstructQueryParameters(parsedArguments);
+            QueryParameters queryParameters = await _cut.ConstructQueryParameters(parsedArguments);
 
             Assert.Multiple(() =>
             {
@@ -69,11 +109,19 @@ namespace bravectlTests
         [InlineData("--filter", "web", "-q", "What")]
         public async Task IsQueryParameterValid_ShallReturnTrue_ForValidQueryParametersObjectAsync(params string[] cmdArguments)
         {
-            _cut = new ConsoleInputService(cmdArguments, _braveAPIService.Object);
-            Dictionary<string, string> parsedArguments = await _cut.ParseCommandLineArguments();
-            QueryParameters queryParameters = await ConsoleInputService.ConstructQueryParameters(parsedArguments);
+            Mock<IBraveAPIService> _braveAPIService = new Mock<IBraveAPIService>();
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
 
-            bool isValid = ConsoleInputService.IsQueryParameterValid(queryParameters, out ICollection<System.ComponentModel.DataAnnotations.ValidationResult> validationResults);
+            Dictionary<string, string> parsedArguments = await _cut.ParseCommandLineArguments();
+            QueryParameters queryParameters = await _cut.ConstructQueryParameters(parsedArguments);
+
+            bool isValid = _cut.IsQueryParameterValid(queryParameters, out ICollection<System.ComponentModel.DataAnnotations.ValidationResult> validationResults);
             Assert.Multiple(() =>
             {
                 Assert.True(isValid, $"Given query-parameters validation check is expected to be true but is was {isValid}.");
@@ -86,19 +134,90 @@ namespace bravectlTests
         [InlineData("--filter", "videos", "-q", "What is your name")]
         public async Task GetSearchResult_ShallReturnValidBraveResponseObjectAsync(params string[] cmdArguments)
         {
+            Mock<IBraveAPIService> _braveAPIService = new();
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
+
             Dictionary<string, string> parsedArguments = await _cut.ParseCommandLineArguments();
-            QueryParameters queryParameters = await ConsoleInputService.ConstructQueryParameters(parsedArguments);
+            QueryParameters queryParameters = await _cut.ConstructQueryParameters(parsedArguments);
             _braveAPIService
                 .Setup(mock => mock.Search(queryParameters).Result)
                 .Returns(new BraveResponse() { Type = $"{cmdArguments[1]}" });
-            _cut = new ConsoleInputService(cmdArguments, _braveAPIService.Object);
-
 
             BraveResponse braveResponse = await _cut.GetSearchResult(queryParameters);
 
             string expectedBraveResponeType = cmdArguments[1];
             string errorMessage = $"Brave-response type is expected to be {expectedBraveResponeType} but it was {braveResponse.Type}.";
             Assert.True(expectedBraveResponeType == braveResponse.Type, errorMessage);
+        }
+
+        [Theory]
+        [InlineData("search", "--filter", "web", "-q", "What is your name")]
+        [InlineData("search", "--filter", "videos", "-q", "What is your name")]
+        public async Task Run_ShallPrint_BraveAPIError_WhenBraveAPIKey_IsMissing(params string[] cmdArguments)
+        {
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            IBraveAPIService _braveAPIService = new BraveAPIService();
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService, console);
+            string sampleConsoleErrorMessage = "Response status code does not indicate success";
+
+            await _cut.Run();
+
+            var result = output.ToString().Trim();
+            string errorDescription = $"Expected ' {sampleConsoleErrorMessage} ' be present in\n\n ' {result} '.";
+            Assert.True(result.IndexOf(sampleConsoleErrorMessage, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
+        }
+
+        [Theory]
+        [InlineData("extract")]
+        public async Task Run_ShallPrint_MissingURLMessage_WhenExtractURL_IsMissing(params string[] cmdArguments)
+        {
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            Mock<IBraveAPIService> _braveAPIService = new();
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
+            string sampleConsoleMessage = "See --help | -h for more info on";
+
+            await _cut.Run();
+
+            var result = output.ToString().Trim();
+            string errorDescription = $"Expect ' {sampleConsoleMessage} ' be present in\n\n ' {result} '";
+            Assert.True(result.IndexOf(sampleConsoleMessage, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
+        }
+
+        [Theory]
+        [InlineData("extract", "https://invalidurl.com")]
+        public async Task Run_ShallPrint_NoContnetMessage_WhenURL_IsInvalid(params string[] cmdArguments)
+        {
+            StringWriter output = new();
+            var console = AnsiConsole.Create(new AnsiConsoleSettings
+            {
+                Ansi = AnsiSupport.No,
+                Out = new AnsiConsoleOutput(output)
+            });
+            Mock<IBraveAPIService> _braveAPIService = new();
+            ConsoleInputService _cut = new(cmdArguments, _braveAPIService.Object, console);
+            string sampleConsoleMessage = "There are no content to extract from";
+
+            await _cut.Run();
+
+            var result = output.ToString().Trim();
+            string errorDescription = $"Expect ' {sampleConsoleMessage} ' be present in\n\n ' {result} '";
+            Assert.True(result.IndexOf(sampleConsoleMessage, StringComparison.OrdinalIgnoreCase) > 0, errorDescription);
         }
     }
 }
